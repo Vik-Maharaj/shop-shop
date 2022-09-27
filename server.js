ed47cab5c8587143318fd8622a094226d903cdc3
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-09-27 14:55:33 -0400

    Udpate server.js to include middleware and server start

--- server.js
+++ server.js
@@ -14,4 +14,29 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
 startApolloServer(typeDefs, resolvers);
