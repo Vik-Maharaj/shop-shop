8e3477a812b6eb1cfafe0da76787e2f8a04a3d2d
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-10-18 16:32:08 -0400

    Add cart component

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,10 @@
+import { useStoreContext } from "../../utils/GlobalState";
+import "./style.css";
+
+const Cart = () => {
+  const [state, dispatch] = useStoreContext();
+
+
+};
+
+export default Cart;
