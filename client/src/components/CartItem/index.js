8c3aeb5ce2c5d7ed4adbc0b91e03ebae13cd9112
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-10-18 19:51:11 -0400

    Add cart item component

---
+++ client/src/components/CartItem/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+
+const CartItem = ({ item }) => {
+
+}
+
+export default CartItem;
