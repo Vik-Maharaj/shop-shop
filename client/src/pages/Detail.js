845fe7d83a48a2b0b886c7e3f8e135b7689516f6
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-10-18 15:59:56 -0400

    Add detail page

---
+++ client/src/pages/Detail.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import Cart from '../components/Cart';
+import { useStoreContext } from '../utils/GlobalState';
+
+import { QUERY_PRODUCTS } from '../utils/queries';
+
+function Detail() {
+  const [state, dispatch] = useStoreContext();
+  const { id } = useParams();
+
+  const [currentProduct, setCurrentProduct] = useState({});
+
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  const { products, cart } = state;
+
+  
+}
+
+export default Detail;
